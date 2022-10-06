dd1a07fd82978da177473685510e09d5a3f96ab6
dolsen0 <derek.olsen@gmail.com>
2022-10-06 13:58:22 -0400

    added links

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
       <h1>Derek Olsen</h1>
       
       <h2>Skills</h2>
+      <p> html, css, js, python, react, nodejs, express, mongodb, gcp</p>
       
       <h2>Resume</h2>
       
@@ -13,12 +14,22 @@ function App() {
       
       <div id='levelup'>
         <h3>Level Up</h3>
-        <p>Image of app</p>
         <p> App to help find time and balance between: mental health, physical health, creative stimuli, and fun   </p>
+        <a href='https://level-up-web.web.app/'>Level Up</a>
       </div>
 
       <h2> Links </h2>
-
+      <ul id='links'>
+      <li>
+      <a href='https://www.bocacode.com/candidates/software-engineer/derek-olsen'>Boca Code</a>
+      </li>
+      <li>
+      <a href='https://github.com/Dolsen0'>Github</a>
+      </li>
+      <li>
+        <a href='https://www.linkedin.com/in/dolsen0/'>Linkedin</a>
+      </li>
+      </ul>
     </div>
   );
 }
